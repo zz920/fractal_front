8128d70b620f552a2017a944e417a257dae87351
zz920 <zz920.zhibowen@gmail.com>
2025-06-26 17:19:19 +0800

    配置GitHub Pages自动部署

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/fractal_front/' : '/',
   server: {
     port: 3000,
+    host: true,
+    open: true
+  },
+  preview: {
+    port: 8080,
     host: true,
     open: true
   },
